2f146021a37f30477612a3f628fd21b0bfd08b31
Francisco Sierra <franjrsierra18@gmail.com>
2019-11-16 23:09:47 +0100

    añadidas vistas de cliente y experto hipotecario. Enlazado el bootstrap

--- resources/views/paginas/cliente.blade.php
+++ resources/views/paginas/cliente.blade.php
@@ -4,11 +4,33 @@
     <h1>Bienvenido cliente!</h1>
     <p>Para solicitar una hipoteca, porfavor rellene el siguiente formulario</p>
     <form action="" method="post">
-        <input type="text">
-        <input type="email" name="" id="">
-        <input type="tel" name="" id="">
-        <input type="number" name="" id="">
-        <input type="number" name="" id="">
-        <input type="submit" value="Solicitar">
+        <div class="form-group">
+            <label for="exampleInputEmail1">Nombre y Apellido</label>
+            <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter nombre">
+            <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+        </div>
+        <div class="form-group">
+            <label for="exampleInputEmail1">Email address</label>
+            <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
+            <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+        </div>
+        <div class="form-group">
+            <label for="exampleInputEmail1">Telefono</label>
+            <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter telefono">
+            <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+        </div>
+        <div class="form-group">
+            <label for="exampleInputPassword1">Ahorros aportados</label>
+            <input type="number" class="form-control" id="exampleInputPassword1" placeholder="Indique sus ahorros">
+        </div>
+        <div class="form-group">
+            <label for="exampleInputPassword">Precio de compra</label>
+            <input type="number" class="form-control" id="exampleInputPassword" placeholder="€€€">
+        </div>
+        <div class="form-check">
+            <input type="checkbox" class="form-check-input" id="exampleCheck1">
+            <label class="form-check-label" for="exampleCheck1">Check me out</label>
+        </div>
+        <button type="submit" class="btn btn-primary">Submit</button>
     </form>
 @endsection
--- resources/views/paginas/index.blade.php
+++ resources/views/paginas/index.blade.php
@@ -1,8 +1,10 @@
 @extends('layouts.app')
 
 @section('content')    
-    <h1>{{$titulo}}</h1>
-    <p>Porfavor indicanos, eres</p>
-    <button><a href="hipotecaApp/public/cliente">Cliente</a></button> 
-    <button><a href="hipotecaApp/public/experto">Experto Hipotecario</a></button>
+    <div class="text-center">
+        <h1>{{$titulo}}</h1>
+        <p class="text-center">Porfavor indicanos, eres</p>
+        <button type="button" class="btn btn-primary"><a href="{{ url('/hipotecaapp/public/cliente')}}" class="text-body">Cliente</a></button> 
+        <button type="button" class="btn btn-success"><a href="hipotecaApp/public/experto" class="text-body">Experto Hipotecario</a></button>
+    </div>
 @endsection
